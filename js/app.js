73d4932a2767b3278e75033f40d342a590050f37
John Rinehart <johnrichardrinehart@gmail.com>
2015-05-29 23:59:26 -0400

    v0.1 of the website. I should remove dependencies (slim down) and clean up the code base. Maybe add AdSense.

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var app = angular.module('ece106',['ngMaterial'])
+
+app.controller('ece106Controller', ['$scope', PostsController = function($scope) {
+        $scope.activeTabs = [];
+        
+        $scope.isOpenTab = function(tab) {
+            if($scope.activeTabs.indexOf(tab) > -1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        $scope.openTab = function(tab) {
+            if($scope.isOpenTab(tab)) {
+                $scope.activeTabs.splice($scope.activeTabs.indexOf(tab), 1);
+            } else {
+                $scope.activeTabs.push(tab);
+            }
+        }
+
+        $scope.docs = {
+            "Doc1": {
+                "title": "Week 1: Quiz 1, Problem 2",
+                "url": "docs/Week1.pdf"
+            },
+            "Post 2": {
+                "title": "Week 2: Gauss's Law Homework Questions",
+                "url": "docs/HomeworkQuestions.pdf"
+            }
+        };
+    }]
+);
