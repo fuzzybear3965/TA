7f520aa4dd3afcc49809047d74b77ca92fbba705
John Rinehart <johnrichardrinehart@gmail.com>
2015-06-04 23:44:56 -0400

    Added document 4. Removed the PayPal links for each document (awkward).

--- js/app.js
+++ js/app.js
@@ -21,17 +21,22 @@ app.controller('ece106Controller', ['$scope', PostsController = function($scope)
 
         $scope.docs = {
             "Doc1": {
-                "title": "Doc. 1, Week 1 - Quiz 1, Problem 2",
+                "title": "Quiz 1 - Problem 2",
                 "url": "docs/Week1.pdf"
             },
             "Post 2": {
-                "title": "Doc. 2, Week 2 - Continuous Charge Homework Questions",
+                "title": "Continuous Charge Homework Questions",
                 "url": "docs/HomeworkQuestions.pdf"
             },
             
             "Post 3": {
-                "title": "Doc. 3, Week 3: Quiz 2 - Rings and Discs of Charge",
+                "title": "Quiz 2 - Rings and Discs of Charge",
                 "url": "docs/RingsAndDiscsOfCharge.pdf"
+            },
+            
+            "Post 4": {
+                "title": "Quiz 3 - Gauss' law",
+                "url": "docs/Week3Quiz3.pdf"
             }
         };
     }]
