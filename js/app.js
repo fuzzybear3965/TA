771f95271b8370355ebff537fc4e204adb9765fb
John Rinehart <johnrichardrinehart@gmail.com>
2015-06-07 22:29:16 -0400

    Quiz #4 solution.

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ app.controller('ece106Controller', ['$scope', PostsController = function($scope)
         }
 
         $scope.docs = {
-            "Doc1": {
+            "Post 1": {
                 "title": "Quiz 1 - Problem 2",
                 "url": "docs/Week1.pdf"
             },
@@ -37,6 +37,11 @@ app.controller('ece106Controller', ['$scope', PostsController = function($scope)
             "Post 4": {
                 "title": "Quiz 3 - Gauss' law",
                 "url": "docs/Week3Quiz3.pdf"
+            },
+            
+            "Post 5": {
+                "title": "Quiz 4 - Taylor Series Expanding a Potential",
+                "url": "docs/Week4Quiz4.pdf"
             }
         };
     }]
